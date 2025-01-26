cbb208c53aee892be50c19ea9c398af303cbbbf8
noraneko498900 <noraneko498900@gmail.com>
2025-01-26 12:31:20 +0900

    kadai_mdn_jsをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+let today = new Date();
+let year = today.getFullYear();
+let month = today.getMonth() + 1;
+let day = today.getDate(); 
+const mdn = () => {
+  console.log(year + '年' + month + '月' + day + '日');
+}
+mdn();
